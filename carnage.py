8e4f3e8ae0687d1c56a801392c0ab9285e5adffe
Артем Насырин <green2023usa@gmail.com>
2025-03-10 12:21:32 +0300

    add docstring

--- carnage.py
+++ carnage.py
@@ -27,12 +27,11 @@ def main(duel_res: str) -> str:
     current_rep = 0.0
     for rep, result, effect in duel_res:
         if result == 'success':
-           current_rep = add_rep (current_rep, rep, effect)
+            current_rep = add_rep (current_rep, rep, effect)
         if result == 'failure':
             current_rep = remove_rep (current_rep, rep, effect)
         return (f'После {len(duel_res)} поединков, репутация персонажа '
                 f'— {current_rep:.3f} очков.')
-    return (f'{len(duel_res)} не было')
 
 
 # Тестовый вызов функции main.
